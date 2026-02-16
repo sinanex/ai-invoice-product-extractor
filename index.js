e4cb69dce29cccd08b7dc45bf908370250826d10
sinan <ms4730435@gmail.com>
2026-02-16 15:00:00 +0530

    updatdd

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const path = require('path');
+require('dotenv').config({
+  path: path.resolve(__dirname, '.env')
+});
+
 const app = require('./app');
-require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running at ${PORT}`);
+  console.log(`Project ID: ${process.env.PROJECT_ID}`);
 });
