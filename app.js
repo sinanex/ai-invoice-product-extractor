8d8ec0f14f8b6077a1693f61f8b1119d3e389eef
Muhammad Sinan <116737048+sinanex@users.noreply.github.com>
2026-02-19 16:39:35 +0530

    Add uptime calculation to health check endpoint

--- app.js
+++ app.js
@@ -9,14 +9,25 @@ app.use(express.json());
 
 app.use('/api', scanRoute);
 
-
 app.get('/health', (req, res) => {
+
+  const uptimeSec = process.uptime();
+
+  const days = Math.floor(uptimeSec / (24 * 60 * 60));
+  const hours = Math.floor((uptimeSec % (24 * 60 * 60)) / (60 * 60));
+  const minutes = Math.floor((uptimeSec % (60 * 60)) / 60);
+  const seconds = Math.floor(uptimeSec % 60);
+
+  const uptime = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+
   res.status(200).json({
     status: "OK",
     message: "Server is running 🚀",
-    uptime: process.uptime(),
+    uptime: uptime,
     timestamp: new Date(),
   });
+
 });
+
 
 module.exports = app;
